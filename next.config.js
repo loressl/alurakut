9c414a9c87880ef070f2c8faa18a389efb164e81
loressl <loryssl@hotmail.com>
2021-07-16 23:36:59 -0300

    Adicionado next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        NEXT_PUBLIC_TOKEN: process.env.NEXT_PUBLIC_TOKEN,
+        NEXT_PUBLIC_URL_DATOCMS: process.env.NEXT_PUBLIC_URL_DATOCMS,
+        NEXT_PUBLIC_ID_MODEL: process.env.NEXT_PUBLIC_ID_MODEL,
+        NEXT_PUBLIC_MY_FOLLOWERS: process.env.NEXT_PUBLIC_MY_FOLLOWERS
+    }
+}
